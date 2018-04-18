439c05f1d002fecdc1a13cd462b30f8bd9889894
atzyrk200 <atzyrk200@caledonian.ac.uk>
2018-04-18 20:31:28 +0100

    asdsad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { NgModule } from "@angular/core";
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {AngularFireStorageModule} from 'angularfire2/storage';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebase: {
+    apiKey: 'AIzaSyDAG2ZRNzb_gxYSyW8y0I53YabhYzSqwz4',
+    authDomain: 'myapp-7bb46.firebaseapp.com',
+    databaseURL: 'https://myapp-7bb46.firebaseio.com',
+    projectId: 'myapp-7bb46',
+    storageBucket: 'gs://myapp-7bb46.appspot.com'
+    ,
+    messagingSenderId: '647153384204'
+  }
 };
